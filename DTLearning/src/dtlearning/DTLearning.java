d7752525f188c513b9bae0ba822f2172ff8f1c62
Anasthasia Amelia <black_spirit_sword@hotmail.com>
2013-09-07 14:21:45 +0700

    first commit

---
+++ DTLearning/src/dtlearning/DTLearning.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtlearning;
+
+/**
+ *
+ * @author Anasthasia
+ */
+public class DTLearning {
+
+    public double countEntropy(double p, double n){
+        double etr = 0;
+        
+        
+        return etr;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
